93869c524e3effbf1f18b984373123c299bd4fcf
justinestilllearning <138890350+justinestilllearning@users.noreply.github.com>
2024-05-01 14:47:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function sweetFunction() {
+    const sweetMessage = document.getElementById("sweet-message");
+    sweetMessage.innerHTML = "<img src='https://i.pinimg.com/originals/f0/b3/87/f0b387aa0215ed48643747a5bea8652f.gif'> <p>iloveyou so much mahal:< here flowers for you poo iloveyouu:< I HOPE NA MAG BATI TAYO MAHAL IM SO SORRY SA GINAWA KO SANA MATANGGAP MO TONG A WAY NA PAG SOSORRY KO NAG E-EFFORT AKO PARA MAGAWA KO TO SANA MAKITA MO SANA PAG GISING KO MAGAAN ULO MO SANA PAG GISING MO BATI NA TAYO ILOVEYOU SO MUCH MAHAL:< SINCERY YOUR BELOVED JUSTINE.</p>";
+}
